ba8fc12bd74f1bb8e5eecfbe1cf0d1d415fcb02f
Vishnu Satish <vishnupavan.satish@gmail.com>
2020-06-09 09:47:24 -0400

    Redirect http to https

--- storefinder/routes.py
+++ storefinder/routes.py
@@ -23,6 +23,13 @@ cloudinary.config(
 )
 
 
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
 @app.route('/')
 @app.route('/home')
 def home():
